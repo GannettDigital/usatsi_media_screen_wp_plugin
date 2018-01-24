c80e81d0d4a9d2a468074303e2fa7ce7ae7049b9
tommydev <tommtyjr700@aol.xom>
2018-01-24 09:50:24 -0500

    resolved jshint errors

--- js/usatsi-media-service.js
+++ js/usatsi-media-service.js
@@ -1,132 +1,148 @@
-"use strict";
+var UsatsiSportsImages = (function () {
+    'use strict';
 
-var UsatsiMexpContentView = wp.media.view.MEXP;
+	if (typeof wp.media.view.MEXP !== "undefined"){
 
-wp.media.view.MEXP = UsatsiMexpContentView.extend(
-	{
+		var UsatsiMexpContentView = wp.media.view.MEXP;
 
-		events: function(){
-			return _.extend(
-				{},UsatsiMexpContentView.prototype.events,{
-					'click .mexp-item' : 'importImage',
-					'click .media-icon-import' : 'triggerImport',
-					'click .media-icon-preview' : 'previewImage',
-					'mouseleave .on-hover-content' : 'closePreview',
-					'click .on-hover-content' : 'triggerImport',
-					'mouseenter .mexp-item' : 'showActions',
-					'mouseleave .mexp-item' : 'hideActions',
-					'click .media-preview-link-copy' : 'copyPreviewLink',
-					'mouseleave .media-preview-link' : 'hidePreviewLink'
-				}
-			);
-		},
+		wp.media.view.MEXP = UsatsiMexpContentView.extend(
+			{
 
-		initialize: function() {
-			// remove the media browser default bindings
-			jQuery( '.mexp-item.attachment' ).unbind();
+				events: function(){
+					return _.extend(
+						{},UsatsiMexpContentView.prototype.events,{
+							'click .mexp-item' : 'importImage',
+							'click .media-icon-import' : 'triggerImport',
+							'click .media-icon-preview' : 'previewImage',
+							'mouseleave .on-hover-content' : 'closePreview',
+							'click .on-hover-content' : 'triggerImport',
+							'mouseenter .mexp-item' : 'showActions',
+							'mouseleave .mexp-item' : 'hideActions',
+							'click .media-preview-link-copy' : 'copyPreviewLink',
+							'mouseleave .media-preview-link' : 'hidePreviewLink'
+						}
+					);
+				},
 
-			// Let's hide the hidden USAT SI tab we dont want users to see it
-			jQuery( '.media-menu-item:contains("USAT Sports Images Hidden")' ).hide();
+				initialize: function() {
+					// remove the media browser default bindings
+					jQuery( '.mexp-item.attachment' ).unbind();
 
-			mexpContentView.prototype.initialize.apply( this, arguments );
-		},
+					// Let's hide the hidden USAT SI tab we dont want users to see it
+					jQuery( '.media-menu-item:contains("USAT Sports Images Hidden")' ).hide();
 
-		copyPreviewLink: function(e) {
-			e.preventDefault();
-			document.execCommand( 'copy', false, jQuery( e.currentTarget ).prev().select() );
-		},
+					mexpContentView.prototype.initialize.apply( this, arguments );
+				},
 
-		showActions: function(e) {
-			e.stopPropagation();
-			jQuery( e.currentTarget ).find( '.media-actions' ).fadeIn( 'fast' );
-		},
+				copyPreviewLink: function(e) {
+					e.preventDefault();
+					document.execCommand( 'copy', false, jQuery( e.currentTarget ).prev().select() );
+				},
 
-		hideActions: function(e) {
-			e.stopPropagation();
-			jQuery( e.currentTarget ).find( '.media-actions' ).fadeOut( 'fast' );
-		},
+				showActions: function(e) {
+					e.stopPropagation();
+					jQuery( e.currentTarget ).find( '.media-actions' ).fadeIn( 'fast' );
+				},
 
-		hidePreviewLink: function(e) {
-			e.stopPropagation();
-			jQuery( e.currentTarget ).fadeOut( 'fast' );
-		},
+				hideActions: function(e) {
+					e.stopPropagation();
+					jQuery( e.currentTarget ).find( '.media-actions' ).fadeOut( 'fast' );
+				},
 
-		closePreview: function(e) {
-			e.stopPropagation();
-			jQuery( e.currentTarget ).fadeOut( 'fast' );
-		},
+				hidePreviewLink: function(e) {
+					e.stopPropagation();
+					jQuery( e.currentTarget ).fadeOut( 'fast' );
+				},
 
-		triggerImport: function(e) {
-			e.stopPropagation();
-			jQuery( e.currentTarget ).parent().parent().find( '.mexp-item-img' ).trigger( 'click' );
-		},
+				closePreview: function(e) {
+					e.stopPropagation();
+					jQuery( e.currentTarget ).fadeOut( 'fast' );
+				},
 
-		previewImage: function(e) {
-			e.stopPropagation();
-			var elm = jQuery( e.currentTarget ).parent().parent().find( '.on-hover-content' );
-			elm.fadeIn(
-				'fast', function() {
-					if ( ! jQuery( this ).hasClass( 'media-loaded' ) ) {
-						jQuery( this ).find( '.media-preview-image' ).attr( 'src', jQuery( this ).find( '.media-preview-image' ).data( 'src' ) );
-					}
-					jQuery( this ).addClass( 'media-loaded' );
-				}
-			);
+				triggerImport: function(e) {
+					e.stopPropagation();
+					jQuery( e.currentTarget ).parent().parent().find( '.mexp-item-img' ).trigger( 'click' );
+				},
 
-		},
-
-		importImage: function(e) {
-			e.preventDefault();
-			e.stopPropagation();
-
-			var imgEl = jQuery( e.currentTarget ).find( '.mexp-item-img' );
-
-			if ( ! jQuery( imgEl ).hasClass( 'media-locked' )  ) {
-
-                // Show downloading spinner!
-                jQuery( imgEl ).parent().parent().siblings( '.mexp-spinner' ).fadeIn( 'fast' );
-
-				var download_url = imgEl.data( 'download-url' ),
-				image_id = imgEl.data( 'image-id' ),
-				post_id = imgEl.data( 'post-id' ),
-				image_title = imgEl.data( 'image-title' ),
-				image_caption = imgEl.data( 'image-caption' ),
-				image_credit = imgEl.data( 'image-credit' ),
-                wp_nonce = imgEl.data( 'nonce' );
-
-				var data = {
-					'action' : 'usatsi_download_image',
-					'download_url' : download_url,
-					'image_id' : image_id,
-					'post_id' : post_id,
-					'image_title' : image_title,
-					'image_caption' : image_caption,
-					'image_credit' : image_credit,
-                    '_wpnonce' : wp_nonce
-				};
-
-				jQuery.post(
-					usatsi_image_ajax.ajax_url + '?_wponce', data, function(response) {
-						usatsi_image_ajax.attachmentId = response;
-
-						// Triggers click on hidden media tab to open edit iframe window
-						jQuery( '.media-menu a:last-child' ).trigger( 'click' );
-
-					}
-				)
-					.fail(
-						function() {
-							console.log( "error" );
+				previewImage: function(e) {
+					e.stopPropagation();
+					var elm = jQuery( e.currentTarget ).parent().parent().find( '.on-hover-content' );
+					elm.fadeIn(
+						'fast', function() {
+							if ( ! jQuery( this ).hasClass( 'media-loaded' ) ) {
+								jQuery( this ).find( '.media-preview-image' ).attr( 'src', jQuery( this ).find( '.media-preview-image' ).data( 'src' ) );
+							}
+							jQuery( this ).addClass( 'media-loaded' );
 						}
 					);
 
-			} else {
-				// Image is not downloadable show messaging overlay!
-				jQuery( imgEl ).parent().parent().siblings( '.media-preview-link' ).fadeIn( 'fast' );
+				},
+
+				importImage: function(e) {
+					e.preventDefault();
+					e.stopPropagation();
+
+					var imgEl = jQuery( e.currentTarget ).find( '.mexp-item-img' );
+
+					if ( ! jQuery( imgEl ).hasClass( 'media-locked' )  ) {
+
+						// Show downloading spinner!
+						jQuery( imgEl ).parent().parent().siblings( '.mexp-spinner' ).fadeIn( 'fast' );
+
+						var download_url = imgEl.data( 'download-url' ),
+							image_id = imgEl.data( 'image-id' ),
+							post_id = imgEl.data( 'post-id' ),
+							image_title = imgEl.data( 'image-title' ),
+							image_caption = imgEl.data( 'image-caption' ),
+							image_credit = imgEl.data( 'image-credit' ),
+							wp_nonce = imgEl.data( 'nonce' );
+
+						var data = {
+							'action' : 'usatsi_download_image',
+							'download_url' : download_url,
+							'image_id' : image_id,
+							'post_id' : post_id,
+							'image_title' : image_title,
+							'image_caption' : image_caption,
+							'image_credit' : image_credit,
+							'_wpnonce' : wp_nonce
+						};
+
+						jQuery.post(
+							usatsi_image_ajax.ajax_url + '?_wponce', data, function(response) {
+								usatsi_image_ajax.attachmentId = response;
+
+								// Triggers click on hidden media tab to open edit iframe window
+								jQuery( '.media-menu a:last-child' ).trigger( 'click' );
+
+							}
+						)
+							.fail(
+								function() {
+									return;
+								}
+							);
+
+					} else {
+						// Image is not downloadable show messaging overlay!
+						jQuery( imgEl ).parent().parent().siblings( '.media-preview-link' ).fadeIn( 'fast' );
+					}
+
+				}
+
+
 			}
-
-		}
-
+		);
 
 	}
-);
+
+});
+
+if (typeof jQuery !== "undefined") {
+
+    jQuery(function() {
+        UsatsiSportsImages();
+    });
+
+}
+
